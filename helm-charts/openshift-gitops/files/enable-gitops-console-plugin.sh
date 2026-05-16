e37a25caa36272c4b36ba3592ffd0e8a7c6a98bc
bkhoo <bkhoo@redhad.com>
2026-05-15 20:04:52 -0400

    change job name

--- helm-charts/openshift-gitops/files/enable-gitops-console-plugin.sh
+++ helm-charts/openshift-gitops/files/enable-gitops-console-plugin.sh
@@ -7,16 +7,3 @@ if [[ ! "gitops-plugin" =~ ${_plugins} ]]; then
   oc patch consoles.operator.openshift.io cluster --type=json \
   --patch='[{"op": "add", "path": "/spec/plugins/-", "value": "gitops-plugin"}]'
 fi
-
-# - name: check console plugins
-#   register: console_plugins
-#   ansible.builtin.command:
-#     cmd: oc get consoles.operator.openshift.io cluster -o jsonpath="{.spec.plugins}"
-
-# - name: ensure gitops console plungin is enabled
-#   when: "'gitops-plugin' not in console_plugins.stdout"
-#   ansible.builtin.command:
-#     # > converts line breaks into space; - removes the trailing newline at the end of the block
-#     cmd: >-
-#       oc patch consoles.operator.openshift.io cluster --type=json
-#       --patch='[{"op": "add", "path": "/spec/plugins/-", "value": "gitops-plugin"}]'
