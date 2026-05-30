37322d4bae30ec0cb6b33e8038247c848056ac5d
bkhoo <bkhoo@redhad.com>
2026-05-30 11:23:50 -0400

    misc updates

--- misc/import-cluster-values.sh
+++ misc/import-cluster-values.sh
@@ -1,20 +1,26 @@
 : << 'COMMENT'
-./misc/import-cluster-values.sh dev-hub01 yes
-./misc/import-cluster-values.sh prod-hub01 yes
-./misc/import-cluster-values.sh prod-cluster01
-./misc/import-cluster-values.sh prod-cluster02
+./misc/import-cluster-values.sh dev-hub01 yes > /tmp/values-dev-hub01.yaml
+./misc/import-cluster-values.sh prod-hub01 yes > /tmp/values-prod-hub01.yaml
+./misc/import-cluster-values.sh prod-cluster01 > /tmp/values-prod-cluster01.yaml
+./misc/import-cluster-values.sh prod-cluster02 > /tmp/values-prod-cluster02.yaml
+
+helm template helm-charts/rhacm-import-cluster/ --values /tmp/values-dev-hub01.yaml | oc apply -f -
+helm template helm-charts/rhacm-import-cluster/ --values /tmp/values-prod-hub01.yaml | oc apply -f -
+helm template helm-charts/rhacm-import-cluster/ --values /tmp/values-prod-cluster01.yaml | oc apply -f -
+helm template helm-charts/rhacm-import-cluster/ --values /tmp/values-prod-cluster02.yaml | oc apply -f -
+
 COMMENT
 
 cluster_name=${1}
 managed_cluster=${2}
 env="${cluster_name%%-*}"
 
-echo "clusterName: $cluster_name" > /tmp/values.yaml
-echo "clusterURL: $(oc whoami --show-server)" >> /tmp/values.yaml
-echo "clusterAuthToken: $(oc whoami --show-token)" >> /tmp/values.yaml
-echo "clusterSet: gitops-clusters" >> /tmp/values.yaml
-echo "clusterLabels:" >> /tmp/values.yaml
-echo "  env: $env" >> /tmp/values.yaml
+echo "clusterName: $cluster_name"
+echo "clusterURL: $(oc whoami --show-server)"
+echo "clusterAuthToken: $(oc whoami --show-token)"
+echo "clusterSet: gitops-clusters"
+echo "clusterLabels:"
+echo "  env: $env"
 if [ -n "$managed_cluster" ]; then
-  echo "  managed-hub: 'yes'" >> /tmp/values.yaml
+  echo "  managed-hub: 'yes'"
 fi
